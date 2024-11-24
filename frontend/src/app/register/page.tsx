22377fb581894f54fb0e7255986fbaead5bfcf87
José Mota <jose.a.mota11@gmail.com>
2024-11-24 19:07:16 -0400

    :lipstick: Styling register page

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,128 @@
+'use client';
+
+import ErrorMessage from '@/components/error-message';
+import { registerSchema } from '@/schemas/register.schema';
+import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Input } from 'antd';
+import Link from 'next/link';
+import {
+  Controller,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+} from 'react-hook-form';
+
+function RegisterPage() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(registerSchema) });
+
+  const onValid: SubmitHandler<FieldValues> = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="h-screen grid place-content-center">
+      <form
+        onSubmit={handleSubmit(onValid)}
+        className="w-[360px] bg-white p-4 rounded-lg shadow-lg flex flex-col gap-4"
+      >
+        <h2 className="text-center text-xl font-semibold">Regístrate</h2>
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <div>
+              <div className="flex items-center gap-3">
+                <UserOutlined style={{ color: '#737373', fontSize: 18 }} />
+                <Input
+                  {...field}
+                  placeholder="example123"
+                  size="large"
+                  status={errors.username && 'error'}
+                />
+              </div>
+              {errors.username && (
+                <ErrorMessage>{errors.username.message as string}</ErrorMessage>
+              )}
+            </div>
+          )}
+        />
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <div>
+              <div className="flex items-center gap-3">
+                <MailOutlined style={{ color: '#737373', fontSize: 18 }} />
+                <Input
+                  {...field}
+                  placeholder="example@gmail.com"
+                  size="large"
+                  status={errors.email && 'error'}
+                />
+              </div>
+              {errors.email && (
+                <ErrorMessage>{errors.email.message as string}</ErrorMessage>
+              )}
+            </div>
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <div>
+              <div className="flex items-center gap-3">
+                <LockOutlined style={{ color: '#737373', fontSize: 18 }} />
+                <Input.Password
+                  {...field}
+                  placeholder="******"
+                  size="large"
+                  status={errors.password && 'error'}
+                />
+              </div>
+              {errors.password && (
+                <ErrorMessage>{errors.password.message as string}</ErrorMessage>
+              )}
+            </div>
+          )}
+        />
+        <Controller
+          name="confirmPassword"
+          control={control}
+          render={({ field }) => (
+            <div>
+              <div className="flex items-center gap-3">
+                <LockOutlined style={{ color: '#737373', fontSize: 18 }} />
+                <Input.Password
+                  {...field}
+                  id="confirmPassword"
+                  placeholder="******"
+                  size="large"
+                  status={errors.confirmPassword && 'error'}
+                />
+              </div>
+              {errors.confirmPassword && (
+                <ErrorMessage>
+                  {errors.confirmPassword.message as string}
+                </ErrorMessage>
+              )}
+            </div>
+          )}
+        />
+        <Button type="primary" htmlType="submit" size="large">
+          Registrarse
+        </Button>
+        <Link href="/login" className="text-center text-blue-400 underline">
+          ¿Ya tienes una cuenta?
+        </Link>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
--- frontend/src/schemas/register.schema.ts
+++ frontend/src/schemas/register.schema.ts
@@ -1,16 +1,29 @@
 import z from 'zod';
 
-export const registerSchema = z.object({
-  username: z
-    .string()
-    .min(3, {
-      message: 'El nombre de usuario debe tener al menos 3 caracteres.',
-    })
-    .max(30, {
-      message: 'El nombre de usuario no puede tener más de 30 caracteres.',
-    }),
-  email: z.string().email({ message: 'El correo electrónico no es válido.' }),
-  password: z
-    .string()
-    .min(6, { message: 'La contraseña debe tener al menos 6 caracteres.' }),
-});
+export const registerSchema = z
+  .object({
+    username: z
+      .string({ required_error: 'Este campo es requerido.' })
+      .min(3, {
+        message: 'El nombre de usuario debe tener al menos 3 caracteres.',
+      })
+      .max(30, {
+        message: 'El nombre de usuario no puede tener más de 30 caracteres.',
+      }),
+    email: z
+      .string({ required_error: 'Este campo es requerido.' })
+      .email({ message: 'El correo electrónico no es válido.' }),
+    password: z
+      .string({ required_error: 'Este campo es requerido.' })
+      .min(6, { message: 'La contraseña debe tener al menos 6 caracteres.' }),
+    confirmPassword: z
+      .string({ required_error: 'Este campo es requerido.' })
+      .min(6, {
+        message:
+          'La confirmación de la contraseña debe tener al menos 6 caracteres.',
+      }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Las contraseñas no coinciden.',
+    path: ['confirmPassword'],
+  });
